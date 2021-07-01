c257d1e11fe90bfdd053fd661c4d7d11c14efd16
unknown <jmcaplan@mail.yu.edu>
2021-07-01 11:36:29 +0300

    finished rc rules

--- BioTapestry2BooleSim/btp2BooleSim.py
+++ BioTapestry2BooleSim/btp2BooleSim.py
@@ -78,49 +78,132 @@ def clean_bool_expression(bool_expression):
 # takes in regulatory conditions specifications, along with the interactions of the node, 
 # and translates those into a boolean expression readable by BooleSim  
 def generate_bool_expression(rc_spec, incoming_edges):
-    bool_expression = symbols('False')
-
-    # categorize nodes by interaction
-    activator_nodes = []
-    repressor_nodes = []
-    neutral_nodes = []
-    for edge in incoming_edges:
-        if edge.get_interaction_num() > 0: activator_nodes.append(edge.get_from_node())
-        elif edge.get_interaction_num() < 0: repressor_nodes.append(edge.get_from_node())
-        else: neutral_nodes.append(edge.get_from_node())
+    bool_expression = symbols('False') # random init
     
-    bool_expression
-    # generate expression based on rc_spec
-    # rc-0: AllActivators AND NoRepressors
+    # AllActivators AND NoRepressors
     if rc_spec == '0':
-        bool_expression = And(get_all_activators_expr(activator_nodes), get_no_repressors_expr(repressor_nodes))
+        bool_expression = And(all_activators_expr(incoming_edges), no_repressors_expr(incoming_edges))
+    
+    # NOT NoActivators AND NoRepressors
+    elif rc_spec == '1':
+        bool_expression = And(Not(no_activators_expr(incoming_edges)),no_repressors_expr(incoming_edges))
+    
+    # AllActivators AND NOT AllRepressors
+    elif rc_spec == '2':
+        bool_expression = And(all_activators_expr(incoming_edges),Not(all_repressors_expr(incoming_edges)))
+    
+    # (NoRepressors AND NOT NoActivators) OR (NOT AllRepressors AND AllActivators)
+    elif rc_spec == '3':
+        lhs = And(no_repressors_expr(incoming_edges),Not(no_activators_expr(incoming_edges)))
+        rhs = And(Not(all_repressors_expr(incoming_edges),all_activators_expr(incoming_edges)))
+        bool_expression = Or(lhs,rhs)
+    
+    # AllActivators
+    elif rc_spec == '4':
+        bool_expression = all_activators_expr(incoming_edges)
+    
+    # AllActivators OR (NoRepressors AND NOT NoActivators)
+    elif rc_spec == '5':
+        lhs = all_activators_expr(incoming_edges)
+        rhs = And(no_repressors_expr(incoming_edges),Not(no_activators_expr(incoming_edges)))
+    
+    # NOT NoActivators AND NOT AllRepressors
+    elif rc_spec == '6':
+        bool_expression = And(Not(no_activators_expr(incoming_edges) , Not(all_repressors_expr(incoming_edges))))
+    
+    # (NOT NoActivators AND NOT AllRepressors) OR AllActivators
+    elif rc_spec == '7':
+        bool_expression = Or(And(no_activators_expr(incoming_edges) , Not(all_repressors_expr(incoming_edges))), all_activators_expr(incoming_edges))
+    
+    # NOT NoActivators
+    elif rc_spec == '8':
+        bool_expression = Not(no_activators_expr(incoming_edges))
+    
+    # NoRepressors
+    elif rc_spec == '9':
+        bool_expression = no_repressors_expr(incoming_edges)
+    
+    # NoRepressors OR (NOT AllRepressors AND AllActivators)
+    elif rc_spec == '10':
+        bool_expression = Or(no_repressors_expr(incoming_edges), And(Not(all_repressors_expr(incoming_edges), all_activators_expr(incoming_edges))))
+    
+    # NoRepressors OR (NOT NoActivators AND NOT AllRepressors)
+    elif rc_spec == '11':
+        bool_expression = Or(no_repressors_expr(incoming_edges), And(Not(no_activators_expr(incoming_edges), Not(all_repressors_expr(incoming_edges)))))
+    
+    # NOT AllRepressors
+    elif rc_spec == '12':
+        bool_expression = Not(all_repressors_expr(incoming_edges))
+    
+    # NOT AllRepressors
+    elif rc_spec == '12':
+        bool_expression = Not(all_repressors_expr(incoming_edges))
+    
+    # NoRepressors OR AllActivators
+    elif rc_spec == '13':
+        bool_expression = Or(no_repressors_expr(incoming_edges), all_activators_expr(incoming_edges))
+    
+    # (NoRepressors OR AllActivators) OR (NOT AllRepressors AND NOT NoActivators)
+    elif rc_spec == '14':
+        bool_expression = Or(Or(no_repressors_expr(incoming_edges), all_activators_expr(incoming_edges)), Or(Not(all_repressors_expr(incoming_edges)), Not(no_activators_expr(incoming_edges))))
+    
+    # NOT AllRepressors OR AllActivators
+    elif rc_spec == '15':
+        bool_expression = Or(Not(all_repressors_expr(incoming_edges)), all_activators_expr(incoming_edges))
+    
+    # NoRepressors OR NOT NoActivators
+    elif rc_spec == '16':
+        bool_expression = Or(no_repressors_expr(incoming_edges), Not(no_activators_expr(incoming_edges)))
+    
+    # NOT AllRepressors OR NOT NoActivators
+    elif rc_spec == '17':
+        bool_expression = Or(Not(all_repressors_expr(incoming_edges)), Not(no_activators_expr(incoming_edges)))
 
     bool_expression = simplify(str(bool_expression))
     return clean_bool_expression(str(bool_expression))
 
-def get_all_activators_expr(activator_nodes):
+'''
+Shortcut methods to retrieve the 4 basic expressions
+'''
+def all_activators_expr(incoming_edges):
+    activator_nodes = get_activator_nodes(incoming_edges)
     bool_expression = symbols('True')
     for i in range(len(activator_nodes)):
         bool_expression = And(bool_expression,symbols(activator_nodes[i]))
-    return simplify(str(bool_expression))
+    return bool_expression
 
-def get_no_activators_expr(activator_nodes):
+def no_activators_expr(incoming_edges):
+    activator_nodes = get_activator_nodes(incoming_edges)
     bool_expression = symbols('True')
     for i in range(len(activator_nodes)):
         bool_expression = And(bool_expression,Not(symbols(activator_nodes[i])))
-    return simplify(str(bool_expression))
+    return bool_expression
 
-def get_all_repressors_expr(repressor_nodes):
+def all_repressors_expr(incoming_edges):
+    repressor_nodes = get_repressor_nodes(incoming_edges)
     bool_expression = symbols('True')
     for i in range(len(repressor_nodes)):
         bool_expression = And(bool_expression,symbols(repressor_nodes[i]))
-    return simplify(str(bool_expression))
+    return bool_expression
 
-def get_no_repressors_expr(repressor_nodes):
+def no_repressors_expr(incoming_edges):
+    repressor_nodes = get_repressor_nodes(incoming_edges)
     bool_expression = symbols('True')
     for i in range(len(repressor_nodes)):
         bool_expression = And(bool_expression,Not(symbols(repressor_nodes[i])))
-    return simplify(str(bool_expression))
+    return bool_expression
+
+def get_activator_nodes(incoming_edges):
+    activator_nodes = []
+    for edge in incoming_edges:
+        if edge.get_interaction_num() > 0: activator_nodes.append(edge.get_from_node())
+    return activator_nodes
+
+def get_repressor_nodes(incoming_edges):
+    repressor_nodes = []
+    for edge in incoming_edges:
+        if edge.get_interaction_num() > 0: repressor_nodes.append(edge.get_from_node())
+    return repressor_nodes
 
 def main():
     # init data structures
