c203e7a1bc08e6509997062c5e9e7c1725dd82da
razielsiegman <rsiegman@mail.yu.edu>
2021-07-08 11:06:34 +0300

    fix typo

--- accuracyTest.bat
+++ accuracyTest.bat
@@ -1,17 +1,17 @@
 cd BREIN_nuSmv_version
 CALL minimal_pl.bat
 cd ../BREIN_nuXmv_version
-CALL minmal_pl.bat
+CALL minimal_pl.bat
 cd ..
 
 cd BREIN_nuSmv_version
 CALL minimal_pl.bat
 cd ../BREIN_nuXmv_version
-CALL minmal_pl.bat
+CALL minimal_pl.bat
 cd ..
 
 cd BREIN_nuSmv_version
 CALL minimal_pl.bat
 cd ../BREIN_nuXmv_version
-CALL minmal_pl.bat
+CALL minimal_pl.bat
 cd ..
